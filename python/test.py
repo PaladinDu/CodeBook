67443cd7df1f985b66462352bf3dd56e6c283faa
杜少成 <paladindu@coconutislandstudio.com>
2023-02-02 16:38:16 +0800

    -

--- python/test.py
+++ python/test.py
@@ -6,7 +6,7 @@
 from code_book import *
 import time
 import json
-
+import binascii
 
 def test(test_code_book, test_key_bytes, test_data_bytes):
     encode_data = test_code_book.encryption(test_data_bytes, test_key_bytes)
@@ -69,7 +69,7 @@ def test_by_test_data(test_data_path):
 
 
 def get_code_by_incr(codebook:CodeBook,num):
-    count = len("ACDEFGHJKLMNPQRSTUVWXYZ23456789")
+    count = len("abcdefghijklmnopqrstuvcxyz1234567890")
     tmp = bytearray("12345678".encode("utf8"))
     tmp_c = 0
     tmp_c2 = 0
@@ -84,17 +84,27 @@ def get_code_by_incr(codebook:CodeBook,num):
     ret = bytes(tmp)
     return ret
 
+meta = "303132333435363738394142434445464748494A4B4C4D4E4F505152535455565758595A6162636465666768696A6B6C6D6E6F707172737475767778797A0E0A33142D0628313A03043D071B38240D1A343205162739150810301E211F2935220F25002A2B010C1D1C2E02113B2337133C2F0B191809122C17202636"
+code_book = CodeBook.init_from_meta_data(meta.encode("utf8"))
+key = code_book.init_seed("coconut".encode("utf8"))
+
+def hex_to_str(str):
+    hex = str.encode('utf-8')
+    str_bin = binascii.unhexlify(hex)
+    return str_bin.decode('utf-8')
+
+def str_to_hex(str):
+    str_bin = str.encode('utf-8')
+    return binascii.hexlify(str_bin).decode('utf-8')
+
+def encode_text(str):
+    hex_str = str_to_hex(str)
+    return code_book.encryption(hex_str.encode("utf8"), key).decode("utf8")
+
+def decode_text(str):
+    origin_str = code_book.decryption(str.encode("utf8"), key).decode("utf8")
+    return hex_to_str(origin_str)
 
 if __name__ == "__main__":
-    # test_data_save_path= "e://cbtest.txt"
-    # test_by_test_data(test_data_save_path)
-    # test_random_data(test_data_save_path)
-    # test_by_test_data(test_data_save_path)
-    code_book = CodeBook.init_from_valid_chars("ACDEFGHJKLMNPQRSTUVWXYZ23456789".encode("utf8"))
-    meta = code_book.get_mate_data().decode("utf8")
-    print(meta)
-    key = code_book.init_seed("coconut".encode("utf8"))
-    for i in range(10000):
-        code = get_code_by_incr(code_book,i)
-        print(code.decode("utf8"))
-        print(code_book.encryption(code,key).decode("utf8"))
+    str = "sb4m8Y5TvludGCbmvdIxCdVJJbCeedkUbSY0AebJiGWFS9kk63V9V4CGyp9jx8P5G7l6OxVgi4qsmeKUTZX9vtM21bPkOYQdKLcmX7kiYdZnu7U005k5d2Gqof3vt4A4yCq4iRt8dambByBMfSAS4ln22Z6INZBgT7WxANisAFGXa19RAiPaJDJKyUS1FUSxp4pl0W30oZ2dGXnIjlQzsTnL6KQviaBt7vZFX6PouNyDpsGEdVdQANJ4EuflR6M1i1EVuPCKacXIEMzlkedXRGhg1uzgN9YYJJppAP8sK5GVLkkWikf1vGbuIdvsKbuBGnyJNoMvQygoCAHb2fylOwrbn6UoUioOivEQg5cjmge5skWBd0QusdBUhF"
+    print(decode_text(str))
